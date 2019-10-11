b5226500791d6c2438401882694b20adceb18f63
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 10:07:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 liams_deli = []
 
+def line
+
+end
+
 def take_a_number(liams_deli, name)
   liams_deli << name
   puts "Welcome, #{name}. You are number #{liams_deli.length} in line."
-end
-
-def line
-
 end
 
 def now_serving
