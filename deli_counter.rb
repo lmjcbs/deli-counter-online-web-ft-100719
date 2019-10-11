438b27ccc2bcc58d486a6729934965fcffcd7b76
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 10:12:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,9 @@ def line(liams_deli)
   if liams_deli.length == 0
     puts "The line is currently empty."
   else
-    print "The line is currently: "
-    liams_deli.each do |name, i|
+    print "The line is currently:"
+    liams_deli.each { |name, i| print " #{i}. #{name}" }
 
-    end
   end
 end
 
